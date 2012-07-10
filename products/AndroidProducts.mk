77c1db9e3089d0ca42b846d6b0f8af5f7100e291
dman3285 <collectiveaocp@gmail.com>
2012-07-10 11:43:49 -0700

    added infuse4g to device tree

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,6 +5,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
+	$(LOCAL_DIR)/infuse4g.mk \
 	$(LOCAL_DIR)/quincyatt.mk \
     $(LOCAL_DIR)/n7000.mk \
     $(LOCAL_DIR)/skyrocket.mk \
