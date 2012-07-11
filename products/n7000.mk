1f762b54746e1e265b43a7246f2b0760dd66ea9f
dman3285 <dman3285@gmail.com>
2012-07-11 00:00:05 -0300

    fixed build errors

--- products/n7000.mk
+++ products/n7000.mk
@@ -1,22 +1,21 @@
-
 # Release name
 PRODUCT_RELEASE_NAME := N7000
 
 # Inherit some common AOKP stuff.
-$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+$(call inherit-product, vendor/aocp/configs/common_phone.mk)
 
 # Add some overlays for toggles
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/n7000
+PRODUCT_PACKAGE_OVERLAYS += vendor/aocp/overlay/n7000
 
 # Inherit GSM common stuff
-$(call inherit-product, vendor/aokp/configs/gsm.mk)
+$(call inherit-product, vendor/aocp/configs/gsm.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
-PRODUCT_NAME := aokp_n7000
+PRODUCT_NAME := aocp_n7000
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-N7000
 
