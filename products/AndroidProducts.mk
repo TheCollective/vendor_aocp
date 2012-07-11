ca7df1af5b56d2f308f09d2efb2fcf94698e7e09
dman3285 <dman3285@gmail.com>
2012-07-10 23:53:53 -0300

    removed crespo4g fixed more build errors

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,12 +1,10 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/crespo.mk \
-    $(LOCAL_DIR)/crespo4g.mk \
     $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
-	$(LOCAL_DIR)/infuse4g.mk \
-	$(LOCAL_DIR)/quincyatt.mk \
+    $(LOCAL_DIR)/infuse4g.mk \
+    $(LOCAL_DIR)/quincyatt.mk \
     $(LOCAL_DIR)/n7000.mk \
     $(LOCAL_DIR)/skyrocket.mk \
     $(LOCAL_DIR)/vibrantmtd.mk \
