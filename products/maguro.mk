5a740a129c4ff8302775e5aa35ba9a49e1b5eef5
dman3285 <dman3285@gmail.com>
2012-07-10 23:59:18 -0300

    fixed build errors

--- products/maguro.mk
+++ products/maguro.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+$(call inherit-product, vendor/aocp/configs/common_phone.mk)
 
 # Inherit GSM common stuff
-$(call inherit-product, vendor/aokp/configs/gsm.mk)
+$(call inherit-product, vendor/aocp/configs/gsm.mk)
 
 # Tuna overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/tuna
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/maguro
+PRODUCT_PACKAGE_OVERLAYS += vendor/aocp/overlay/tuna
+PRODUCT_PACKAGE_OVERLAYS += vendor/aocp/overlay/maguro
 
 # Setup device specific product configuration.
-PRODUCT_NAME := aokp_maguro
+PRODUCT_NAME := aocp_maguro
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := maguro
 PRODUCT_MODEL := Galaxy Nexus
@@ -22,7 +22,7 @@ PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=yakju BUILD_FINGERPRINT=google/yakj
 
 # Copy maguro specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/aokp/prebuilt/tuna/Thinkfree.apk:system/app/Thinkfree.apk \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip \
-    vendor/aokp/prebuilt/tuna/vold.fstab:system/etc/vold.fstab
+    vendor/aocp/prebuilt/tuna/Thinkfree.apk:system/app/Thinkfree.apk \
+    vendor/aocp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip \
+    vendor/aocp/prebuilt/tuna/vold.fstab:system/etc/vold.fstab
 
