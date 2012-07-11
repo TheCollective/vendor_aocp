b2fcc5808310b788312ccbb751b6aa7f0af6b48d
dman3285 <dman3285@gmail.com>
2012-07-10 23:55:05 -0300

    fixed build errors

--- products/crespo.mk
+++ products/crespo.mk
@@ -2,16 +2,16 @@
 $(call inherit-product, device/samsung/crespo/full_crespo.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+$(call inherit-product, vendor/aocp/configs/common_phone.mk)
 
 # Inherit GSM common stuff
-$(call inherit-product, vendor/aokp/configs/gsm.mk)
+$(call inherit-product, vendor/aocp/configs/gsm.mk)
 
 # Common overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/crespo
+PRODUCT_PACKAGE_OVERLAYS += vendor/aocp/overlay/crespo
 
 # Setup device specific product configuration.
-PRODUCT_NAME := aokp_crespo
+PRODUCT_NAME := aocp_crespo
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := crespo
 PRODUCT_MODEL := Nexus S
@@ -21,6 +21,5 @@ PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=soju BUILD_FINGERPRINT=google/soju/
 
 # Copy crespo specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/aokp/prebuilt/crespo/Thinkfree.apk:system/app/Thinkfree.apk \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_480_800.zip:system/media/bootanimation.zip
+    vendor/aocp/prebuilt/bootanimation/bootanimation_480_800.zip:system/media/bootanimation.zip
 
