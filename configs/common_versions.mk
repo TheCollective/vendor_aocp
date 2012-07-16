81848e293de57a217f647413c34defd60c564296
dman3285 <dman3285@gmail.com>
2012-07-15 21:15:20 -0300

    added overrides for build prop rom and device specific properties

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,14 +1,18 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-# Rom Manager properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=aocp \
-    ro.aocp.version=$(TARGET_PRODUCT)_cpv-1
+PRODUCT_VERSION_MAJOR = 1
+PRODUCT_VERSION_MINOR = 0
+PRODUCT_VERSION_MAINTENANCE = CP-1
+PRODUCT_VERSION_GOO =1
+PRODUCT_RELEASE_NAME = I717
 
-# Goo updater app
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.developerid=aocp \
-    ro.goo.rom=aocp \
-    ro.goo.version=19
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76L
 
+# Rom manager properties
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.modversion=aocp-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).
+$(PRODUCT_VERSION_MAINTENANCE)-$(PRODUCT_RELEASE_NAME) \
+ro.goo.developerid=aocp \
+ro.goo.rom=aocp \
+ro.goo.version=$(PRODUCT_VERSION_GOO)
