ad03ba41713ced248beae799b2640b249279134b
ruger94 <tmurry56@gmail.com>
2012-08-10 16:21:08 -0500

    Update vendorsetup.sh
    added user-debug to d2att

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ add_lunch_combo aocp_maguro-userdebug
 add_lunch_combo aocp_crespo-userdebug
 add_lunch_combo aocp_crespo4g-userdebug
 add_lunch_combo aocp_captivatemtd-userdebug
-add_lunch_combo aocp_d2att
+add_lunch_combo aocp_d2att-userdebug
 add_lunch_combo aocp_fascinatemtd-userdebug
 add_lunch_combo aocp_galaxysmtd-userdebug
 add_lunch_combo aocp_quincyatt-userdebug
