bc724c3d7ad42b98f194454f75b0bda8a6a0df68
Johnsondr80 <Johnsondr80@gmail.com>
2012-08-11 18:18:23 -0500

    update common make to build Nfc

--- configs/common.mk
+++ configs/common.mk
@@ -7,7 +7,8 @@ PRODUCT_PACKAGES += \
     PhaseBeam \
     su \
     AppWidgetPicker \
-    openvpn \ 
+    openvpn \
+    Nfc \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
