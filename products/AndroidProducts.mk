6060cbd1e25509f1db931ecf3ff607262741673f
dman3285 <dman3285@gmail.com>
2012-08-18 02:15:35 -0300

    add to call on infuse and captivate

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,6 +1,8 @@
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/d2att.mk \
+    $(LOCAL_DIR)/infuse4g.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/quincyatt.mk \
  
