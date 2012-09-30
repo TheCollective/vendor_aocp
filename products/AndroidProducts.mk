00fb1372db1f29c787cdff74d7433d955ee26feb
TheCollective <thecollective@thecollective-VirtualBox.(none)>
2012-09-30 14:15:38 -0500

    'add skyrocket'

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,6 +5,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/infuse4g.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/quincyatt.mk \
+    $(LOCAL_DIR)/skyrocket.mk \
     $(LOCAL_DIR)/toro.mk \
  
   
