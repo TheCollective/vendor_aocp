cfdf05e04c753199d6f8a9babda5a5ef0daa0949
Johnsondr80 <Johnsondr80@gmail.com>
2012-10-14 12:42:34 -0500

    update

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -6,13 +6,13 @@ DATE = $(shell date +%h-%d-%y)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.aocp.version=$(TARGET_PRODUCT)-$(PRODUCT_VERSION_MAINTENANCE)-$(DATE)
 
-PRODUCT_VERSION_MAINTENANCE = CP-3
+PRODUCT_VERSION_MAINTENANCE = CP-4
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aocp \
     ro.goo.rom=aocp \
-    ro.goo.version=3
+    ro.goo.version=4
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
