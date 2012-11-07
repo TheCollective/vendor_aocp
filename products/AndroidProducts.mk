96b83c9b4aac2b61c3bff8f34da1a7af83fc5d9e
dman3285 <dman3285@gmail.com>
2012-11-07 14:18:40 -0500

    Remove ace.mk and glide.mk(Accidental Push)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,10 +1,8 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ace.mk \
     $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/d2att.mk \
     $(LOCAL_DIR)/infuse4g.mk \
-    $(LOCAL_DIR)/glide.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/quincyatt.mk \
     $(LOCAL_DIR)/skyrocket.mk \
