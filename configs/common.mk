90140822b845236acefebf85d074841381161a2e
Johnsondr80 <Johnsondr80@gmail.com>
2012-11-08 00:31:08 -0600

    correct poor spelling

--- configs/common.mk
+++ configs/common.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
         vendor/aocp/prebuilt/common/app/com.adobe.air-1.apk:system/app/com.adobe.air-1.apk \
         vendor/aocp/prebuilt/common/app/com.adobe.flashplayer-1.apk:system/app/com.adobe.flashplayer-1.apk \
         vendor/aocp/prebuilt/common/app/FileManager.apk:system/app/FileManager.apk \
-        vendor/aocp/prebuilt/common/app/TitanumBackup.apk:system/app/TitanumBackup.apk \
+        vendor/aocp/prebuilt/common/app/TitaniumBackup.apk:system/app/TitaniumBackup.apk \
         vendor/aocp/prebuilt/common/app/LatinImeDictionaryPack.apk:system/app/LatinImeDictionaryPack.apk
 
 PRODUCT_PROPERTY_OVERRIDES += \
