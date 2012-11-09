4770de21effb757f51313bd23d039dbd30f96f42
AOSP-ZEUS <dman3285@gmail.com>
2012-11-08 23:55:51 -0500

    remove products as we build from device tree now

--- configs/common.mk
+++ configs/common.mk
@@ -17,21 +17,11 @@ PRODUCT_PACKAGES += \
     MusicVisualization \
     NoiseField \
     openvpn \
-    QuietTime \
     SuperSU 
 
 # Use prebuilt su until fixed when built
 PRODUCT_COPY_FILES += \
     vendor/aocp/prebuilt/common/xbin/su:system/xbin/su
-
-# PackagesAppsPrebuilts that common for all devices
-PRODUCT_COPY_FILES += \
-        vendor/aocp/prebuilt/common/app/AoCPLiveWall.apk:system/app/AoCPLiveWall.apk \
-        vendor/aocp/prebuilt/common/app/com.adobe.air-1.apk:system/app/com.adobe.air-1.apk \
-        vendor/aocp/prebuilt/common/app/com.adobe.flashplayer-1.apk:system/app/com.adobe.flashplayer-1.apk \
-        vendor/aocp/prebuilt/common/app/FileManager.apk:system/app/FileManager.apk \
-        vendor/aocp/prebuilt/common/app/TitaniumBackup.apk:system/app/TitaniumBackup.apk \
-        vendor/aocp/prebuilt/common/app/LatinImeDictionaryPack.apk:system/app/LatinImeDictionaryPack.apk
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
