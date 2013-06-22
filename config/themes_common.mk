0fb99da93928905426fe09d1c8ef6ae6a06c9439
Roger Michaud <cptjohn47@gmail.com>
2013-06-22 10:35:56 -0500

    Add boot animation for SGS4

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGES += \
        ThemeManager \
        ThemeChooser \
        com.tmobile.themes \
-       AoCP_Icon_Pack
+       AoCP_Icon_Pack 
 
 PRODUCT_COPY_FILES += \
        vendor/aocp/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
