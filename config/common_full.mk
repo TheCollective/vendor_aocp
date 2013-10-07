a4cabf377d1a0d98084f35b3a485c56bbf97fa44
fezzy <fezzy102@gmail.com>
2013-10-07 18:40:30 -0400

    'Forgot to push the rest of the change for the rework  of the sound files, and a few tweaks to squisher and optimizer.:)

--- config/common_full.mk
+++ config/common_full.mk
@@ -2,15 +2,13 @@
 $(call inherit-product, vendor/aocp/config/common.mk)
 
 # Bring in all audio files
-include frameworks/base/data/sounds/OriginalAudio.mk
+include frameworks/base/data/sounds/NewAudio.mk
 
 # Extra Ringtones
-# include frameworks/base/data/sounds/AudioPackageNewWave.mk
+include frameworks/base/data/sounds/AudioPackageNewWave.mk
 
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
-
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
